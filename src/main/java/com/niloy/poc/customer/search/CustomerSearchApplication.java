b47c83ac85d54d0851272a20290ea5b2d3697cfd
Niloy Saha <32936923+sahaniloy@users.noreply.github.com>
2019-06-30 21:34:30 -0700

    initial commit

---
+++ src/main/java/com/niloy/poc/customer/search/CustomerSearchApplication.java
@@ -0,0 +1,13 @@
+package com.niloy.poc.customer.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/niloy/poc/customer/search/CustomerSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.niloy.poc.customer.search;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CustomerSearchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
